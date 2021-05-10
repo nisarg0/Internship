8617fc1cbbb4c73f0f6e2e4d52ec5a5c769a4f1c
nisarg0 <nisarggogate212@gmail.com>
2021-05-10 22:48:05 +0530

    :whale: Started

--- Web scrapping/login_page_scrapper.py
+++ Web scrapping/login_page_scrapper.py
@@ -27,7 +27,7 @@ def find_fieldnames(url):
         'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
         'Chrome/90.0.4430.93 Safari/537.36'
     }
-    response = get(url)
+    response = get(url,header = usr_agent)
     # response.raise_for_status()
     parse_html(response.text)
 
