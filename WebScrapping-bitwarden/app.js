bd3cd0e7c999b5a13d5970da85294b88f35c0aba
nisarg0 <nisarggogate212@gmail.com>
2021-05-11 15:41:12 +0530

    :pencil: send html dom page done

--- WebScrapping-bitwarden/app.js
+++ WebScrapping-bitwarden/app.js
@@ -1,39 +1,38 @@
+const pageParser = require("./pageParser");
 const axios = require("axios");
 const http = require("http");
 const bodyParser = require("body-parser");
 const express = require("express");
+const { response } = require("express");
 const app = express();
 
-// Let this eb the url we got from client
-const URL = "https://www.facebook.com/";
-
-// fetch_h;
-
-var getForm = async (url) => {
+var GetHtmlPage = async (url) => {
   axios
     .get(url)
     .then((response) => {
-      // console.log(typeof response.data);
-      return response.data;
+      // console.log(response.data);
+      const data = response.data;
+      return data;
+    })
+    .then((data) => {
+      pageParser.parsepage(data);
     })
     .catch((error) => {
       console.log(error);
     });
+  // return data;
 };
-
-// getForm(URL);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/", (req, res) => {
   console.log("URL : " + req.body.siteurl);
+  // URL I recieve from site
+  var url = req.body.siteurl;
 
-  var content = getForm(URL);
+  GetHtmlPage(url);
 
-  console.log(typeof content);
-  console.log(content);
   res.send("Success");
-  // res.redirect('/');
 });
 
 app.listen(54321);
