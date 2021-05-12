6cba8b89884bf90bab72faaffd4069eacf6655ca
nisarg0 <nisarggogate212@gmail.com>
2021-05-12 16:53:50 +0530

    trying

--- WebScrapping-bitwarden/pageParser.js
+++
@@ -1,9 +0,0 @@
-const DomParser = require("dom-parser");
-
-function parsepage(data) {
-  // console.log(data);
-  const parser = new DomParser();
-  const document = parser.parseFromString(data, "text/hmtl");
-
-  console.dir(document);
-}
